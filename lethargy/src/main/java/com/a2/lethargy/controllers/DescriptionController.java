a44f6cdc42b16de2c639a5e15b808b9c498bbe2d
Yuri-IQI <anthony.yuri.ff@gmail.com>
2025-02-11 08:24:34 -0300

    AAAAA

--- lethargy/src/main/java/com/a2/lethargy/controllers/DescriptionController.java
+++ lethargy/src/main/java/com/a2/lethargy/controllers/DescriptionController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.a2.lethargy.dtos.DescriptionDto;
@@ -33,7 +33,7 @@ public class DescriptionController {
 	}
 	
 	@GetMapping("/{key}")
-	public ResponseEntity<List<Object>> getAllDescriptions(@RequestParam String key) {
+	public ResponseEntity<List<Object>> getAllDescriptions(@PathVariable String key) {
 		return new ResponseEntity<>(descriptionService.getAllDescriptions(key), HttpStatus.OK);
 	}
 }
